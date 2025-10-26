6655e0db6a493e72ddb9c3c7896e9f5a668d2762
HOTP1E <korchuganov.artem@yandex.ru>
2025-10-27 00:06:07 +0300

    стер первую строку из этого файла

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,5 +1,28 @@
 package entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column
+    private String userName;
+    @Column
+    private String email;
+    @Column
+    private String lastCard;
+    @Column
+    private Timestamp lastEntry;
 }
--- src/main/resources/db/migration/V1_1_0__init.sql
+++ src/main/resources/db/migration/V1_1_0__init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE cards (
     id BIGSERIAL PRIMARY KEY,
-    card_title VARCHAR(50) NOT NULL,
-    description VARCHAR(1000) NOT NULL
+    card_title VARCHAR(50),
+    description VARCHAR(1000)
 );
